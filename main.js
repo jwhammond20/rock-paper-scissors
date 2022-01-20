3f1a699496aebf22adf6691da4f0fa53aad735e7
jwhammond20 <jwhammond20@gmail.com>
2022-01-20 16:43:21 -0500

    cleaned up UI

--- main.js
+++ main.js
@@ -17,85 +17,81 @@ const gameOptions = ['rock', 'paper', 'scissors'];
 
 // Random Computer Selection
 function computerPlay(arr) {
-     const randomIndex = Math.floor(Math.random() * arr.length);
-     const computerPick = arr[randomIndex];
-     return computerPick 
-     }
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  const computerPick = arr[randomIndex];
+  return computerPick
+}
 
 
 // Player vs computer function with score++
 function playRound(playerSelection, computerSelection) {
-        if (playerSelection === computerSelection) {
-        draws++;
-        winner.innerHTML = `
+  if (playerSelection === computerSelection) {
+    draws++;
+    winner.innerHTML = `
         <h3>${draw}</h3>`;
-        return draw;
-        } 
-        else if (playerSelection === 'rock' && computerSelection === 'scissors'){
-        playerScore++;
-        winner.innerHTML = `<h3>${playerWinRound}</h3>`;
-        return playerWinRound;
-        } 
-        else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        playerScore++;
-        winner.innerHTML = `<h3>${playerWinRound}</h3>`;
-        return playerWinRound;
-        }
-        else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        playerScore++;
-        winner.innerHTML = `<h3>${playerWinRound}</h3>`;
-        return playerWinRound;
-        }
-        else {
-        computerScore++;
-        winner.innerHTML = `<h3>${computerWinRound}</h3>`;
-        return computerWinRound
-        }
-    }
-    
+    return draw;
+  } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    playerScore++;
+    winner.innerHTML = `<h3>${playerWinRound}</h3>`;
+    return playerWinRound;
+  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    playerScore++;
+    winner.innerHTML = `<h3>${playerWinRound}</h3>`;
+    return playerWinRound;
+  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    playerScore++;
+    winner.innerHTML = `<h3>${playerWinRound}</h3>`;
+    return playerWinRound;
+  } else {
+    computerScore++;
+    winner.innerHTML = `<h3>${computerWinRound}</h3>`;
+    return computerWinRound
+  }
+}
+
 
 // Main Game Play - started from click event. ends after winner = 5.
 function gamePlay(e) {
-        const playerSelection = e.target.id;
-        const computerSelection = computerPlay(gameOptions);
-        console.log(`My-Pick: ${playerSelection} vs Computer-Pick: ${computerSelection}`)
-        console.log(playRound(playerSelection, computerSelection));
-        console.log(`Wins: ${playerScore} - Draws: ${draws} - Losses: ${computerScore}`)
-        keepScore();
-        showPicks(playerSelection, computerSelection);
-    }
+  const playerSelection = e.target.id;
+  const computerSelection = computerPlay(gameOptions);
+  console.log(`My-Pick: ${playerSelection} vs Computer-Pick: ${computerSelection}`)
+  console.log(playRound(playerSelection, computerSelection));
+  console.log(`Wins: ${playerScore} - Draws: ${draws} - Losses: ${computerScore}`)
+  keepScore();
+  showPicks(playerSelection, computerSelection);
+}
 
- // display Score on screen    
- function keepScore() {
-    myScore.innerHTML = playerScore;
-    compScore.innerHTML = computerScore;
-    tieGame.innerHTML = draws;
- }
+// display Score on screen    
+function keepScore() {
+  myScore.innerHTML = playerScore;
+  compScore.innerHTML = computerScore;
+  tieGame.innerHTML = draws;
+}
 
 
- function showPicks(playerSelection, computerSelection) {
-     userPick.innerHTML = `
-     <h4>Player: ${playerSelection}</h4>
+function showPicks(playerSelection, computerSelection) {
+  userPick.innerHTML = `
+     <h4>User: ${playerSelection}</h4>
      <i class="icon2 far fa-hand-${playerSelection} fa-9x"></i>`;
-     
-     compPick.innerHTML = `
+
+  compPick.innerHTML = `
      <h4>Comp: ${computerSelection}</h4>
      <i class="icon2 far fa-hand-${computerSelection} fa-9x"></i>
      `;
-     
-     modal.style.display = 'block';
- }
 
- // Clear modal
+  modal.style.display = 'block';
+}
+
+// Clear modal
 function clearModal(e) {
-    if (e.target == modal) {
-      modal.style.display = 'none';
-    }
+  if (e.target == modal) {
+    modal.style.display = 'none';
   }
+}
 
 function nextRound() {
-    modal.style.display = 'none';
-}  
+  modal.style.display = 'none';
+}
 
 function restartGame() {
   playerScore = 0;
@@ -104,7 +100,7 @@ function restartGame() {
   myScore.innerHTML = playerScore;
   compScore.innerHTML = computerScore;
   tieGame.innerHTML = draws;
-}  
+}
 
 // outputs for return variables
 let playerWinRound = "You Win"
